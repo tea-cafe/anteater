c628800bb50e9e497aa6c6926233524560a8f634
tea-cafe <tea_cafe@163.com>
2017-11-28 20:54:40 +0800

    ok

--- ui/controllers/bg/ApiSession.php
+++ ui/controllers/bg/ApiSession.php
@@ -9,7 +9,7 @@ class ApiSession extends MY_Controller {
         $_md = '{"apps":"android,cn.coupon.kfc,cn.coupon.mac,cn.wps.moffice_eng,com. MobileTicket,com.UCMobile,com.alipay.security.mobile.authenticator,co m.android.BBKClock,com.android.BBKCrontab,com.android.BBKPhoneIn structions,com.android.BBKTools,com.android.VideoPlayer,com.android. attachcamera,com.android.backupconfirm,com.android.bbk.lockscreen3 ","idfa":"AEBE52E7-03EE-455A-B3C4-E57283966239","imei":"355065053 311001","latitude":"104.07642","longitude":"38.6518","model":"MIMAX", "nt":"wifi","os":"Android","os_version":"7","vendor":"Xiaomi"}';
 		$url = 'https://engine.tuia.cn/index/activity?appKey=%s&adslotId=%s&md=%s&timestamp=%s&nonce=%s&signature=%s';
 		$appKey = '2i9jsRZNd6rdMzDPoduqm9ZQswqx';
-		$adslotId = '2982';
+		$adslotId = '2700';
         $md = base64_encode(gzencode($_md, 9));
         $timestamp = time();
         $nonce = rand(pow(10, 5), pow(10, 6)-1);
