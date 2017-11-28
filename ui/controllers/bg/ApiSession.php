1df8bf63bc493a0883593524e46173f0268fc1ad
sweifan <345627116@qq.com>
2017-11-28 20:53:17 +0800

    /x_api/UserLogin/checkUserLogin ok

--- ui/controllers/bg/ApiSession.php
+++ ui/controllers/bg/ApiSession.php
@@ -8,12 +8,12 @@ class ApiSession extends MY_Controller {
     public function gen_session() {
         $_md = '{"apps":"android,cn.coupon.kfc,cn.coupon.mac,cn.wps.moffice_eng,com. MobileTicket,com.UCMobile,com.alipay.security.mobile.authenticator,co m.android.BBKClock,com.android.BBKCrontab,com.android.BBKPhoneIn structions,com.android.BBKTools,com.android.VideoPlayer,com.android. attachcamera,com.android.backupconfirm,com.android.bbk.lockscreen3 ","idfa":"AEBE52E7-03EE-455A-B3C4-E57283966239","imei":"355065053 311001","latitude":"104.07642","longitude":"38.6518","model":"MIMAX", "nt":"wifi","os":"Android","os_version":"7","vendor":"Xiaomi"}';
 		$url = 'https://engine.tuia.cn/index/activity?appKey=%s&adslotId=%s&md=%s&timestamp=%s&nonce=%s&signature=%s';
-		$appKey = '3fhAGjn23sFoMqHmTFDYGsLfXsUj';
+		$appKey = '2i9jsRZNd6rdMzDPoduqm9ZQswqx';
 		$adslotId = '2982';
         $md = base64_encode(gzencode($_md, 9));
         $timestamp = time();
         $nonce = rand(pow(10, 5), pow(10, 6)-1);
-        $appSecret = '3XXL8XNJZvD3duhPPL6zomrAP8KF4X49n1SZKwe';
+        $appSecret = '3WjQ2DZ1HFMjAt3occZzxWcsKwNXDVVMCVKCvLR';
         $strHash = "appSecret={$appSecret}&md={$md}"."&"."timestamp={$timestamp}&nonce={$nonce}";
 		$signature = base64_encode(sha1($strHash, true));
         $api_url = sprintf($url, $appKey, $adslotId, urlencode($md), $timestamp, $nonce, $signature);
