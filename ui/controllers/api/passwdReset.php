1b985b5ddb3c8ae160efa5cbf106159f47c56579
yuzhouoo <719559506@qq.com>
2017-11-29 22:15:09 +0800

    修正接口返回方式

---
+++ ui/controllers/api/passwdReset.php
@@ -0,0 +1,21 @@
+<?php
+//defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * 重置密码
+ */
+
+class passwdReset extends CI_Controller{
+	public function __construct(){
+		parent::__construct();
+		echo '1111111111';
+		exit;
+	}
+
+	public function index(){
+		echo 11111111111111;
+		exit;
+		//var_dump($_POST);	
+	}
+}
+?>
